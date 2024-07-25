ba0aa3313a1ebab66ae88389a2200d300d7824e5
Shaurya144 <chundawatshaurya@gmail.com>
2024-07-25 13:34:03 +0100

    Add files via upload

---
+++ GamePanel.java
@@ -0,0 +1,42 @@
+import java.awt.event.*;
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+
+public class GamePanel {
+	GamePanel(){
+		
+	}
+	public void newBall() {
+		
+	}
+	
+	public void newPaddles() {
+		
+	}
+	
+	public void paint(Graphics g) {
+		
+	}
+	
+	public void draw(Graphics g) {
+		
+	}
+	
+	public void move() {
+		
+	}
+	
+	public void checkCollision() {
+		
+	}
+	
+	public void Run() {
+		
+	}
+	
+	public class AL extends KeyAdapter{
+		public void
+		)
+	}
+}
---
+++ Padles.java
@@ -0,0 +1,10 @@
+import java.awt.event.*;
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+
+public class Padles {
+	Padles() {
+		
+	}
+}
